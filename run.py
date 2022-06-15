35d07d2ea84d05184e197652ffc6ddb10bb70c18
Jisk Attema <j.attema@esciencecenter.nl>
2022-06-15 21:34:30 +0200

    BUGFIX: run.py

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
-# import os
-# os.system('python3 compass/visualisation.py')
-
 import sys
 from bokeh.plotting import curdoc
 
-sys.path.insert(0, "compass")
-from visualisation import BokehServer
+from compass.visualisation import BokehServer
 
 server = BokehServer(curdoc())
 server.run_visualisation()
