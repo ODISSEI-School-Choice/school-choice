fd772f3db1546945cbcf4d46c4cf25d6a7d38b55
jiqicn <qiji1988ben@gmail.com>
2022-10-13 16:42:22 +0200

    Update setup.py to update package name to 'compass-school'.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="school-choice",
+    name="compass-school",
     version="v1.0.1",
     description="This Python implementation tries to model school choice and resulting school segregation based on the work of Schelling (1971) and Stoica & Flache (2014).",
     author="Eric Digum, Jisk Attenma, Ji Qi",
@@ -18,17 +18,17 @@ setup(
     url="https://github.com/ODISSEI-School-Choice/school-choice",
     download_url="https://github.com/ODISSEI-School-Choice/school-choice/archive/refs/tags/v1.0.0.tar.gz",
     install_requires=[
-        "bokeh==2.4.3",
-        "geopandas==0.11.1",
-        "hypothesis==6.56.2",
-        "ijson==3.1.4",
-        "Mesa==1.1.0",
-        "pytest==7.1.3",
-        "scikit_learn==1.1.2",
-        "scipy==1.9.2",
-        "seaborn==0.12.0",
-        "Shapely==1.8.4",
-        "dask==2022.9.2",
-        "distributed=2022.9.2",
+        "bokeh",
+        "hypothesis",
+        "ijson",
+        "Mesa",
+        "pytest",
+        "scikit_learn",
+        "scipy",
+        "seaborn",
+        "dask",
+        "distributed",
+        "geopandas",
+        "shapely"
     ],
 )
